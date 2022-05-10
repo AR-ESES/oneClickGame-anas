af00199ff4847de596a55654ce834189f4d349b6
anacarolinafeio <102221959+anacarolinafeio@users.noreply.github.com>
2022-05-10 21:15:23 +0100

    Add files via upload

---
+++ predio.js
@@ -0,0 +1,19 @@
+class Predio {
+  constructor() {
+    this.r = 75;
+    this.x = width;
+    this.y = height - this.r;
+  }
+
+  move() {
+    this.x -= 16;
+  }
+
+  show() {
+    image(pImg, this.x, this.y, this.r, this.r);
+
+    // fill(255, 50);
+    // ellipseMode(CORNER);
+    // ellipse(this.x, this.y, this.r, this.r);
+  }
+}
